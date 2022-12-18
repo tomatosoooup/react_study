d894904f91b08abe2c98ee232673b1eacf785a7d
tomatosoooup <v.gabinet02@icloud.com>
2022-12-18 23:47:16 +0200

    new project with fetch requests

--- the_scenes_of_react/src/App.js
+++ the_scenes_of_react/src/App.js
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
-
+import React, { useState, useCallback } from "react";
+import Button from "./components/UI/Button/Button";
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
-function App() {
-  const [,] = useState();
+const App = () => {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log("APP RUNNING");
+
+  const changeShowParagraph = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle((prevToggle) => !prevToggle);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <p>This is new</p>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
+      <Button onClick={changeShowParagraph}>Click to show</Button>
     </div>
   );
-}
+};
 
 export default App;
