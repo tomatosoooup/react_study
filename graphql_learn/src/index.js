60aa06449fb4a2c5f29067bf7b5dea821029b513
Vasyl <v.gabinet02@icloud.com>
2023-12-03 18:21:35 +0200

    fixed errors with versions

--- graphql_learn/src/index.js
+++ graphql_learn/src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-
 import { ApolloProvider } from "@apollo/client";
 
 import client from "./apollo/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
